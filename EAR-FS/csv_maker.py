370eef61ad698e8eb0cd42d1748f19273e52ec4b
Prakash-Jugunta <Prakashjugunta132005@gmail.com>
2025-05-05 21:34:34 +0530

    DATASET

--- EAR-FS/csv_maker.py
+++ EAR-FS/csv_maker.py
@@ -1,4 +1,4 @@
 import pandas as pd
-data=pd.read_excel('dataset02.xlsx')
+data=pd.read_excel('DCT_orginal_Telugu_test.xlsx')
 print(data.columns)
-data.to_csv('data.csv')
+data.to_csv('DCT_orginal_Telugu_test.csv')
